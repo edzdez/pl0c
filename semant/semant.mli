d6bfd6c42da1f2d36241459d7cd28bfa9944e36d
Ethan Zhang <ethanz4@illinois.edu>
2025-12-29 18:57:58 -0600

    update responisiblities of typecheck: in particular, checknig kinds now belongs to elab

--- semant/semant.mli
+++ semant/semant.mli
@@ -3,6 +3,6 @@
  *
  * This phase accepts the parse tree as input, and consists of the following processes:
  *  - Elaboration: annotate the AST with semantic information.
- *  - Typechecking: check expression types and kinds.
+ *  - Typechecking: check expression types.
  *  - Constant Folding: evaluate constant expressions at compile time.
  *)
